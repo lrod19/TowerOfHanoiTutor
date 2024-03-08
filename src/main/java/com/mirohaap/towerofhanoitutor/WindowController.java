119b0c018b1cbf7c8442eceac6c3d246c5545090
Luis Rodriguez <94203328+lrod19@users.noreply.github.com>
2024-03-07 18:30:21 -0800

    updated the background

--- src/main/java/com/mirohaap/towerofhanoitutor/WindowController.java
+++ src/main/java/com/mirohaap/towerofhanoitutor/WindowController.java
@@ -2,13 +2,17 @@ package com.mirohaap.towerofhanoitutor;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 public class WindowController {
     @FXML
-    private Label welcomeText;
+    private TextFlow tutorText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Hello world");
+    protected void onRestartButtonClick() {
+        tutorText.getChildren().remove(test);
+        tutorText.getChildren().add(test);
     }
+    private Text test = new Text("Testing Testing 123");
 }
