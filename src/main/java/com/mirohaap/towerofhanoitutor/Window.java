0211d64c80bab9d10623afca58e2019646628852
Luis Rodriguez <94203328+lrod19@users.noreply.github.com>
2024-03-07 16:57:20 -0800

    Update Window.java

--- src/main/java/com/mirohaap/towerofhanoitutor/Window.java
+++ src/main/java/com/mirohaap/towerofhanoitutor/Window.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class Window extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Window.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Window.class.getResource("hanoi-tower.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hanoi Tower");
         stage.setScene(scene);
         stage.show();
     }
