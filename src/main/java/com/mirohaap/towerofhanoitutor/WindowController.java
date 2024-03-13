d0bb16a7fdfaa9d706fd2bcd1b6c6feef35f1edf
Luis Rodriguez <94203328+lrod19@users.noreply.github.com>
2024-03-13 16:09:46 -0700

    Update WindowController.java

--- src/main/java/com/mirohaap/towerofhanoitutor/WindowController.java
+++ src/main/java/com/mirohaap/towerofhanoitutor/WindowController.java
@@ -1,25 +1,39 @@
 package com.mirohaap.towerofhanoitutor;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class WindowController {
     @FXML
     private TextFlow tutorText;
+    private int index = 0;
 
     @FXML
-    protected void onRestartButtonClick() {
-        Text test = new Text(String.format("%f", Math.random()));
-        tutorText.getChildren().clear();
-        tutorText.getChildren().add(test);
+    protected void onRestartButtonClick() throws IOException {
+        index = 0;
+        if (Tutor.getTutor().isEnabled()){
+            Tutor.getTutor().toggleEnabled();
+        }
+        Stage stage = (Stage) tutorText.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(Window.class.getResource("hanoi-tower.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 790, 620);
+        stage.setScene(scene);
     }
     @FXML
     protected void onTutorToggled() {
         Tutor.getTutor().toggleEnabled();
         tutorText.getChildren().clear();
-        Text test = new Text(String.format("%s", Tutor.getTutor().isEnabled()));
-        tutorText.getChildren().add(test);
+        if (Tutor.getTutor().isEnabled()){
+            Text test = new Text(Tutor.getTutor().getBestMoves().get(index).toString());
+            tutorText.getChildren().add(test);
+            index += 1;
+        }
     }
 }
